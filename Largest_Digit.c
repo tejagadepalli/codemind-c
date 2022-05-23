e40cc7e7eb1f96b02c602b58835ac9fc15b10861
tejagadepalli <97437250+tejagadepalli@users.noreply.github.com>
2022-05-23 11:13:31 +0530

    #include<stdio.h> int main() {     int num,remider,largest=0;     scanf("%d",&num);     while(num>0)     {         remider=num%10;         if (largest<remider)         {             largest=remider;         }         num=num/10;         }         printf("%d",largest);         return 0; }

--- Largest_Digit.c
+++ Largest_Digit.c
@@ -4,15 +4,14 @@ int main()
     int num,remider,largest=0; 
     scanf("%d",&num); 
     while(num>0) 
-    {
-        remider=num%10; 
-        if (largest<remider)
+    { 
+        remider=num%10;
+        if (largest<remider) 
         { 
             largest=remider; 
         } 
-        num=num/10; 
-        
-    } 
-    printf("%d",largest);
-    return 0; 
+        num=num/10;
+        } 
+        printf("%d",largest); 
+        return 0; 
 }
