ad08d5891564317ec7acc1dfbe0474c968561c10
tejagadepalli <97437250+tejagadepalli@users.noreply.github.com>
2022-05-23 11:31:23 +0530

    #include<stdio.h> int main() {     int a,b,c,next,num;     scanf("%d", &num);     if((num==0)||(num==1))     printf(" %d is a Fibonacci term",num);     else     {         a=0; b=1; c=a+b;         while(c<num)         {             a=b; b=c; c=a+b;         }         if(c==num)         printf("True",num);         else         printf("False",num);     }     return 0; }

--- Fibonacci_or_not.c
+++ Fibonacci_or_not.c
@@ -1,25 +1,22 @@
 #include<stdio.h>
 int main()
-{
-    int n,a,b,c;
-    scanf("%d",&n);
-    a=0;
-    b=1;
-    if(n==0)
-      printf("True");
-    c=a+b;
-    while(c<n)
-    {
-        c=a+b;
-        a=b;
-        b=c;
-    }
-    if(c==n)
-    {
-        printf("True");
-    }
+{ 
+    int a,b,c,next,num; 
+    scanf("%d", &num); 
+    if((num==0)||(num==1)) 
+    printf("
+%d is a Fibonacci term",num);
     else
-    {
-        printf("False");
-    }
+    { 
+        a=0; b=1; c=a+b; 
+        while(c<num) 
+        {
+            a=b; b=c; c=a+b; 
+        } 
+        if(c==num) 
+        printf("True",num); 
+        else 
+        printf("False",num); 
+    } 
+    return 0;
 }
