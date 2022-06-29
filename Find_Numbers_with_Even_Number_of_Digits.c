5d57dee0df65f363b7830d778525c27bd0e725c8
tejagadepalli <97437250+tejagadepalli@users.noreply.github.com>
2022-06-29 11:34:14 +0530

    #include<stdio.h> int main(){ int a,i,c=0; scanf("%d",&a); int r[a]; for(i=0;i<a;i++) { scanf("%d",&r[i]); } for(i=0;i<a;i++) {int t=0; while(r[i]!=0) { r[i]/=10; t++; } if(t%2==0) c++; } printf("%d",c); return 0;}

---
+++ Find_Numbers_with_Even_Number_of_Digits.c
@@ -0,0 +1,2 @@
+#include<stdio.h>
+int main(){ int a,i,c=0; scanf("%d",&a); int r[a]; for(i=0;i<a;i++) { scanf("%d",&r[i]); } for(i=0;i<a;i++) {int t=0; while(r[i]!=0) { r[i]/=10; t++; } if(t%2==0) c++; } printf("%d",c); return 0;}
