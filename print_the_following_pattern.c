94abecfb646153c84365b4eafd83361b70aeb5ef
tejagadepalli <97437250+tejagadepalli@users.noreply.github.com>
2022-06-29 10:01:12 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=i;j++)         {             printf("%c ",(i+64));         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int i,j,n;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=n;i>=1;i--)
     {
         for(j=1;j<=i;j++)
         {
-            printf("%d",j);
+            printf("%c ",(i+64));
         }
         printf("
 ");
