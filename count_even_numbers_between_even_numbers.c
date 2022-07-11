37cf9654b46274dea7182310bdbcbca61380294e
tejagadepalli <97437250+tejagadepalli@users.noreply.github.com>
2022-07-11 20:48:19 +0530

    #include<stdio.h> int main() {     int n,c=0,s;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {             c++;             s=c/2;         }     }     printf("%d",s); }

---
+++ count_even_numbers_between_even_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,c=0,s;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            c++;
+            s=c/2;
+        }
+    }
+    printf("%d",s);
+}
