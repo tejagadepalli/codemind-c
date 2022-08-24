9c054e9278211ac52b6673a34c7071460eea7c4d
tejagadepalli <97437250+tejagadepalli@users.noreply.github.com>
2022-08-24 10:59:35 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         for(int j=i+1;j<n;j++)         {             if(a[i]==a[j])             {                 printf("%d",a[i]);             }         }     } }

---
+++ Find_the_Duplicate_Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                printf("%d",a[i]);
+            }
+        }
+    }
+}
